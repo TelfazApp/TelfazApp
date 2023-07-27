0c2aa1bce660dcbaa328afa86f3d26f7cb2a06eb
FaselApp <140677270+FaselApp@users.noreply.github.com>
2023-07-27 19:21:10 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,10 +2,11 @@
 const firebaseConfig = {
   apiKey: "AIzaSyAc4r2MjNmiPF-35vlx4H-HGcmKMw0nCGc",
   authDomain: "databasemjeed.firebaseapp.com",
-  databaseURL: "https://databasemjeed.firebaseio.com",
   projectId: "databasemjeed",
+  storageBucket: "databasemjeed.appspot.com",
   messagingSenderId: "1032574225285",
   appId: "1:1032574225285:web:80c5ee27d4ad82f6a312ec",
+  measurementId: "G-REJX6J2YGC"
 };
 
 firebase.initializeApp(firebaseConfig);
