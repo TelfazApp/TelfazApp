62c71c4c29179e742330fd93de97476ee80ef497
FaselApp <140677270+FaselApp@users.noreply.github.com>
2023-07-28 07:39:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,56 @@
-// Get a reference to the Firestore database
-const db = firebase.firestore();
+// Import necessary Firebase modules
+import { initializeApp } from 'firebase/app';
+import { getFirestore, setDoc, doc } from 'firebase/firestore';
 
-// Function to generate a random alphanumeric code
+// Your Firebase configuration directly in app.js (replace the placeholders with your actual Firebase config)
+const firebaseConfig = {
+  apiKey: 'AIzaSyAc4r2MjNmiPF-35vlx4H-HGcmKMw0nCGc',
+  authDomain: 'databasemjeed.firebaseapp.com',
+  projectId: 'databasemjeed',
+  storageBucket: 'databasemjeed.appspot.com',
+  messagingSenderId: '1032574225285',
+  appId: '1032574225285',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+
+// Function to generate a random alphanumeric code (same as before)
 function generateCode(length) {
-  // ... (same as the previous code)
+  // ...
 }
 
-// Function to add a new code with an expiration date of 30 days from the activation date
+// Function to add a new code with an expiration date of 30 days from the activation date (same as before)
 async function addCode() {
-  const code = generateCode(15);
-  const expirationDate = new Date();
-  expirationDate.setDate(expirationDate.getDate() + 30);
-
-  try {
-    await db.collection('codes').doc(code).set({ expirationDate: expirationDate.getTime(), activated: false });
-    return { code, expirationDate };
-  } catch (error) {
-    console.error('Error adding code:', error);
-    return null;
-  }
+  // ...
 }
 
-// Function to display the generated code and expiration date
-function displayCode(codeData) {
+// Function to display the generated codes on the webpage (same as before)
+function displayCodes(codesData) {
   const outputDiv = document.getElementById('output');
+  outputDiv.innerHTML = ''; // Clear the previous codes
 
-  if (codeData) {
-    const { code, expirationDate } = codeData;
-    outputDiv.innerHTML = `
-      <p>Generated Code: ${code}</p>
-      <p>Expiration Date: ${expirationDate}</p>
-    `;
+  if (codesData.length > 0) {
+    codesData.forEach((codeData) => {
+      const { code, expirationDate } = codeData;
+      const codeElement = document.createElement('p');
+      codeElement.innerHTML = `Generated Code: ${code}<br>Expiration Date: ${expirationDate}`;
+      outputDiv.appendChild(codeElement);
+    });
   } else {
-    outputDiv.innerHTML = '<p>Error generating code.</p>';
+    outputDiv.innerHTML = '<p>No codes generated yet.</p>';
   }
-}
 
-// Function to handle button click and generate code
-document.getElementById('generateButton').addEventListener('click', async () => {
-  const codeData = await addCode();
-  displayCode(codeData);
+  // Print "Button Pressed!" message to the outputDiv
+  const messageElement = document.createElement('p');
+  messageElement.textContent = 'Button Pressed!';
+  outputDiv.appendChild(messageElement);
+}
+// Wait for DOM content to load before setting up event listeners
+document.addEventListener('DOMContentLoaded', () => {
+  // Function to handle button click and generate codes (same as before)
+  document.getElementById('generateButton').addEventListener('click', async () => {
+    // ...
+  });
 });
